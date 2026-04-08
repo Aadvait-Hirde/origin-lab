643518d60ed6acbcf65f7fed484cdb9808b3bbdc
Aadvait Hirde <ahirde@iu.edu>
2026-04-07 21:09:32 -0400

    tweak: added apply route

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: "/apply",
+        destination: "https://airtable.com/applefttOQAtaZ7wa/paggfdS3QhArLUQxt/form",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
